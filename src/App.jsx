27b7d3ef66c7be066bf9b28cd56989b40a0d192b
manu-saini94 <manu.saini94@gmail.com>
2023-11-07 15:15:23 +0530

    game history implemented

--- src/App.jsx
+++ src/App.jsx
@@ -6,22 +6,28 @@ import { calculateWinner } from './winner';
 import StatusMessage from './components/StatusMessage';
 import History from './components/History';
 
+const NEW_GAME = [{squares:[null,null,null,null,null,null,null,null,null],isNext:false}];
 
 function App()  {
-  const [history, setHistory] = useState([{squares:[null,null,null,null,null,null,null,null,null],isNext:false}]);
+  const [history, setHistory] = useState(NEW_GAME);
 
 
   const [currentMove,setCurrentMove] = useState(0);
   const gamingBoard = history[currentMove];
 
-
+console.log(gamingBoard,"gamingBoard")
   const winner = calculateWinner(gamingBoard.squares);
   console.log(history,currentMove);
 
   const handleSquareClick = clickedPosition => {
+    
     if (gamingBoard.squares[clickedPosition] || winner) return;
     setHistory(currentHistory => {
-      const lastGamingState = history[history.length - 1];
+
+      const isTraversing = currentMove + 1 !== currentHistory.length
+
+      const lastGamingState = isTraversing ? currentHistory[currentMove] : currentHistory[history.length - 1];
+
       const nextSquaresState = lastGamingState.squares.map(
         (squareValue, position) => {
           if (clickedPosition === position)
@@ -29,7 +35,9 @@ function App()  {
           return squareValue;
         }
       );
-      return currentHistory.concat({
+
+      const base = isTraversing ? currentHistory.slice(0,currentHistory.indexOf(lastGamingState)+1) : currentHistory;
+      return base.concat({
         squares: nextSquaresState,
         isNext: !lastGamingState.isNext,
       });
@@ -46,6 +54,7 @@ function App()  {
     <div className="app">
     <StatusMessage winner={winner} gamingBoard={gamingBoard} />
      <Board squares={gamingBoard.squares} handleSquareClick={handleSquareClick}/>  
+     <h2>Current Game History</h2>
      <History history={history} currentMove={currentMove}  moveTo={moveTo} />
     </div>
   );
--- src/components/History.jsx
+++ src/components/History.jsx
@@ -3,7 +3,7 @@ return (
     <div className="history-wrapper">
         <ul className="history">
             {history.map((abc,index) => {
-                <li key={index}>
+               return  <li key={index}>
                   <button type="button" className={`btn-move ${currentMove === index ? 'active' : '' }`} onClick={() => moveTo(index)}
                   >
                     {index === 0 ? 'Go to game start' : `Go to move ${index}`}
